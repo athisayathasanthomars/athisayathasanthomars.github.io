bb7fd5453c8ab786a593d83b07a424c7a52957b2
Athisayathasan Thomars Thuvaranagan <94943914+athisayathasanthomars@users.noreply.github.com>
2023-12-15 22:05:53 +0530

    Update home.js

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,11 @@
+function openNav() {
+    document.getElementById("sidebar").style.left = "0";
+    document.getElementById("bars").style.display = "none";
+    document.getElementById("times").style.display = "block";
+}
 
+function closeNav() {
+    document.getElementById("sidebar").style.left = "-210px";
+    document.getElementById("bars").style.display = "block";
+    document.getElementById("times").style.display = "none";
+}
